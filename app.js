ef1ab45eb8c58e81891022f9c2f5e85d0b3eaa08
augustoflores <augustoflores@gmail.com>
2019-05-30 09:52:40 -0500

    variables fix

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 var jsonFormat = require('./node_modules/json-format');
 
-let arrMentores = [];
 let csv = fs.readFileSync('./file/mentorsList.csv', 'utf8');
 csv = csv.split("\n");
 csv.shift();
 
-arrMentores =csv.map(elem => {
+let arrMentores =csv.map(elem => {
     elem = elem.split(',')
     return { "name": elem[0], "course": elem[1] } 
 })
